9c902104ec3682ffed9c53b27f16f97aaa242f06
shipuhimel <shipuhimel@gmail.com>
2020-12-09 12:23:42 +0600

    set up

---
+++ app/Jobs/SendEmailJob.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Jobs;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldBeUnique;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class SendEmailJob implements ShouldQueue
+{
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    protected $details;
+    /**
+     * Create a new job instance.
+     *
+     * @return void
+     */
+    public function __construct($details)
+    {
+        $this->details = $details;
+        //
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        $email = new SendEmailTest();
+        Mail::to($this->details['emails'])->send($email);
+
+    }
+}
---
+++ resources/views/emails/demoMail.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>mailQuee</title>
+</head>
+<body>
+
+<h1>Hello Dear</h1>
+<p>We are happy to notify you that , you have passed the test.</p>
+<br>
+<h6>Regards</h6>
+mailQuee test team.
+</body>
+</html>
